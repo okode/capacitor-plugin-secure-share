68a72bf495ead8b2b716c2e4ba3a57e3fa07589d
Pedro Jorquera <pjorquera@okode.com>
2022-10-24 14:06:12 +0200

    Fixed Android tests

--- android/src/androidTest/java/com/okode/secureshare/ExampleInstrumentedTest.java
+++ android/src/androidTest/java/com/okode/secureshare/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.okode.secureshare", appContext.getPackageName());
+        assertEquals("com.okode.secureshare.test", appContext.getPackageName());
     }
 }
